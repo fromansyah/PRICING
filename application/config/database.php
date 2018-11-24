d5010c075681a81ef823a9ee0a3e7821d203990b
fromansyah <45254852+fromansyah@users.noreply.github.com>
2018-11-25 00:41:37 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -73,7 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = "default";
 $active_record = TRUE;
 
-$db['default']['hostname'] = "pricing-app.azurewebsites.net";
+$db['default']['hostname'] = "pricing-db-server.mysql.database.azure.com";
 $db['default']['username'] = "fromansyah";
 $db['default']['password'] = "Neverl4nd";
 $db['default']['database'] = "pricing";
