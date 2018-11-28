a783f6a5fa9f5f13293a1d4eecfd85cb89afd683
fromansyah <45254852+fromansyah@users.noreply.github.com>
2018-11-28 12:18:07 +0700

    Update log_file.php

--- application/views/product/log_file.php
+++ application/views/product/log_file.php
@@ -11,7 +11,7 @@ var _base_url = '<?= base_url() ?>';
 
 function back()
 {
-    window.location = _base_url + 'Product/new_upload_product/';
+    window.location = _base_url + 'index.php/Product/new_upload_product/';
 }
 
 </script>
