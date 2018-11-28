79e585d92322390dae743fe2b8a444061b2d32ee
fromansyah <45254852+fromansyah@users.noreply.github.com>
2018-11-28 12:14:13 +0700

    Update form_template.php

--- application/views/form_template.php
+++ application/views/form_template.php
@@ -5,11 +5,11 @@
 <link href="<?= base_url() ?>css/datePicker.css" rel="stylesheet" type="text/css" />
 <script src="<?= base_url() ?>js/jquery.datePicker.js" type="text/javascript"></script>
 <script src="<?= base_url() ?>js/jquery-1.8.0.min.js" type="text/javascript"></script>
-<link href="<?=$this->config->item('base_url');?>css/style.css" rel="stylesheet" type="text/css" />
+<link href="<?=$this->config->item('base_url');?>/css/style.css" rel="stylesheet" type="text/css" />
 
 <link href="<?= base_url() ?>css/new_menu.css" rel="stylesheet" type="text/css" />
 <link href="<?= base_url() ?>css/backoff_ssek.css" rel="stylesheet" type="text/css" />
-<link href="<?=$this->config->item('base_url');?>css/style.css" rel="stylesheet" type="text/css" />
+<link href="<?=$this->config->item('base_url');?>/css/style.css" rel="stylesheet" type="text/css" />
 <script src="<?= base_url() ?>js/new_menu.js" type="text/javascript"></script>
 
 <link rel="icon" type="image/gif" href="<?= base_url() ?>images/icon_ecolab.jpg" /> 
