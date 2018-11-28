2a2a35f1694a5b9c049c0d13393528d5ab73a20e
fromansyah <45254852+fromansyah@users.noreply.github.com>
2018-11-28 14:54:04 +0700

    Update log_file.php

--- application/views/sales/log_file.php
+++ application/views/sales/log_file.php
@@ -11,7 +11,7 @@ var _base_url = '<?= base_url() ?>';
 
 function back()
 {
-    window.location = _base_url + 'Sales/new_upload_sales/';
+    window.location = _base_url + 'index.php/Sales/new_upload_sales/';
 }
 
 </script>
