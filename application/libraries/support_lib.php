685cce4aac27a4360ae344cf592a1171856d6f3d
fromansyah <45254852+fromansyah@users.noreply.github.com>
2018-12-11 00:18:55 +0700

    Update support_lib.php

--- application/libraries/support_lib.php
+++ application/libraries/support_lib.php
@@ -6,7 +6,8 @@ class support_lib
         $this->CI=& get_instance();
         $this->CI->load->library('session');
         $this->CI->load->helper('form');
-        $this->CI->load->helper('url');        
+        $this->CI->load->helper('url');     
+        $this->CI->load->library('aad_auth');
     }
 
     function check_login() {
