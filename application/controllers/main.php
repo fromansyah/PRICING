eaf61da10a7cd343bdfdee30786c3fe5f4579564
fromansyah <45254852+fromansyah@users.noreply.github.com>
2018-12-11 16:50:42 +0700

    Update main.php

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -14,7 +14,11 @@ public function __construct()
         if ($this->session->userdata("username")):
             redirect("index.php/Menu_utama","refresh");
         else:
-            $data["title"] = "Login";
+             $data = array(
+                'user_info' => $this->aad_auth->user_info(),
+                'id_token'  => $this->aad_auth->id_token(),
+            );
+	    
             $data["content"] = $this->load->view('vlogin',$data,true);
             $this->load->view("login", $data);
         endif;
