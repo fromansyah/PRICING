deaaf69681be713d798d0871acbbd687332bc113
fromansyah <45254852+fromansyah@users.noreply.github.com>
2018-12-11 15:49:50 +0700

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -112,9 +112,9 @@ class Welcome extends CI_Controller {
         $this->session->unset_userdata('nik');
         $this->session->unset_userdata('emp_id');
 	
-	$return_to = $this->input->get('return_to');
-        $this->aad_auth->logout($return_to === NULL ? site_url() : $return_to);
-	//$this->aad_auth->logout();
+	//$return_to = $this->input->get('return_to');
+        //$this->aad_auth->logout($return_to === NULL ? site_url() : $return_to);
+	$this->aad_auth->logout();
 	    
 	
         
