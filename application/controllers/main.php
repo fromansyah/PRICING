d820b923cdfe329712e0cc422dd49d723f06de70
fromansyah <45254852+fromansyah@users.noreply.github.com>
2018-12-11 16:54:24 +0700

    Update main.php

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -16,6 +16,28 @@ public function __construct()
         $error = $this->input->get('error');
         $code = $this->input->get('code');
 	    
+	// Regardless if authentication was successful or not, the state value MUST be the expected one.
+        if ($this->session->aad_auth_nonce === NULL || $this->session->aad_auth_nonce !== $state)
+        {
+            die('State value returned (\'' . $state . '\') is not the value expected (\''
+                 . $this->session->aad_auth_nonce . '\').');
+        }
+        else
+        {
+            if ($error !== NULL || $code === NULL)
+            {
+                // Error during authentication
+                echo '<pre>' . $error . '</pre>';
+                echo '<pre>' . $this->input->get('error_description') . '</pre>';
+            }
+            else
+            {
+                // Successful authentication, now use the authentication code to get an Access Token and ID Token
+                echo '<pre>'; var_dump($this->input->get()); echo '</pre>';
+                $this->aad_auth->request_tokens($this->input->get('code'), $this->session->aad_auth_nonce);
+            }
+        }
+	    
         if ($this->session->userdata("username")):
             redirect("index.php/Menu_utama","refresh");
         else:
