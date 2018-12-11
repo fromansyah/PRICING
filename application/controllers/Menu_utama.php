964ac39f53a005c0e68640c83546dfb266160874
fromansyah <45254852+fromansyah@users.noreply.github.com>
2018-12-11 15:31:51 +0700

    Update Menu_utama.php

--- application/controllers/Menu_utama.php
+++ application/controllers/Menu_utama.php
@@ -21,32 +21,6 @@ class Menu_utama extends CI_Controller {
     }
     
     public function index(){
-       /*$this->load->library('session');
-        $state = $this->input->get('state');
-        $error = $this->input->get('error');
-        $code = $this->input->get('code');
-        // Regardless if authentication was successful or not, the state value MUST be the expected one.
-        if ($this->session->aad_auth_nonce === NULL || $this->session->aad_auth_nonce !== $state)
-        {
-            die('State value returned (\'' . $state . '\') is not the value expected (\''
-                 . $this->session->aad_auth_nonce . '\').');
-        }
-        else
-        {
-            if ($error !== NULL || $code === NULL)
-            {
-                // Error during authentication
-                echo '<pre>' . $error . '</pre>';
-                echo '<pre>' . $this->input->get('error_description') . '</pre>';
-            }
-            else
-            {
-                // Successful authentication, now use the authentication code to get an Access Token and ID Token
-                echo '<pre>'; var_dump($this->input->get()); echo '</pre>';
-                $this->aad_auth->request_tokens($this->input->get('code'), $this->session->aad_auth_nonce);
-            }
-        }*/
-        
         $data['title'] = 'Main Menu';
         
         $notification = $this->Plan_model->getPlanNotificationList();
