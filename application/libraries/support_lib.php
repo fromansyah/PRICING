8462aad25a2a683bdbc0a7d8be0123069b08c53a
fromansyah <45254852+fromansyah@users.noreply.github.com>
2018-12-11 16:47:11 +0700

    Update support_lib.php

--- application/libraries/support_lib.php
+++ application/libraries/support_lib.php
@@ -22,7 +22,7 @@ class support_lib
             if ($this->CI->session->userdata('username')) {
                 return true;
             } else {
-                redirect('index.php/Welcome');
+                redirect('index.php/Main');
                 //redirect('https://www.office.com/');
                 return false;
             }
