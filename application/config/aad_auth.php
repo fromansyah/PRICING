dd5e839e6ed8479134e1e411b34ae0894d7bf8ef
fromansyah <45254852+fromansyah@users.noreply.github.com>
2018-12-11 14:28:28 +0700

    Update aad_auth.php

--- application/config/aad_auth.php
+++ application/config/aad_auth.php
@@ -13,14 +13,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * E.g. $config['directory_identifier'] = '2916ea73-ecdf-4ed4-94ad-4a30a6e7a3c3';
  * E.g. $config['directory_identifier'] = 'contoso.com';
  */
-$config['directory_identifier'] = '<your directory identifier or domain name>';
+$config['directory_identifier'] = 'firatromansyahgmail.onmicrosoft.com';
 
 /**
  * The client ID of the application (as registered in Azure AD).
  *
  * E.g. $config['client_id'] = '2916ea73-ecdf-4ed4-94ad-4a30a6e7a3c3';
  */
-$config['client_id'] = 'f9381a38-ba6a-401a-95c7-3d47abc8789e';
+$config['client_id'] = '3f4ef0cc-e639-474a-9abf-8e94c1491ee9';
 
 
 /**
@@ -28,7 +28,7 @@ $config['client_id'] = 'f9381a38-ba6a-401a-95c7-3d47abc8789e';
  *
  * E.g. $config['client_secret'] = '9ltvIfj9zVISfYyR5oEdmBmbnDBsEPDAZpJeURgpidEj';
  */
-$config['client_secret'] = 'O;qu^2k&!E_{U=?;/';
+$config['client_secret'] = 'RMGNC1Uv5QDHA6PAAI2F5OaCEzcpVvZfTKIaY3/lboo=';
 
 /**
  * The segment of this application that will be used as the redirect URI.
@@ -36,7 +36,7 @@ $config['client_secret'] = 'O;qu^2k&!E_{U=?;/';
  * This will be used as the parameter to site_url().
  * E.g. $config['redirect_uri_segment'] = 'sample/handle_response';
  */
-$config['redirect_uri_segment'] = '<URI segment to the response handler>';
+$config['redirect_uri_segment'] = 'index.php/Menu_utama';
 
 
 
