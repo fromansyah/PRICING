b3a320f57f30f2d47b8ede1629e7e1b53ee65d83
fromansyah <45254852+fromansyah@users.noreply.github.com>
2019-01-15 15:09:53 +0700

    Update Role_menu.php

--- application/controllers/Role_menu.php
+++ application/controllers/Role_menu.php
@@ -44,7 +44,7 @@ class Role_menu extends CI_Controller {
             $menu = $this->Dyn_menu_model->getMenuById($role_menu->menu_id)->result();
 		
             $button = '';
-            if($this->session->userdata("role") == 1 || $this->session->userdata("role") == 3){
+            if($this->session->userdata("role") == 1){
 		    $button = '<a href=\'#\' onclick="edit_role_menu(\''.$role_menu->role_id.'\''.',\''.$role_menu->menu_id.'\')"><img border=\'0\' src=\''.$this->config->item('base_url').'/images/file_edit.png\'></a>'.'&nbsp&nbsp&nbsp'.
 			      '<a href=\'#\' onclick="delete_role_menu(\''.$role_menu->role_id.'\''.',\''.$role_menu->menu_id.'\''.',\''.$menu[0]->title.'\')"><img border=\'0\' src=\''.$this->config->item('base_url').'/images/file_delete.png\'></a>';
 	    }
