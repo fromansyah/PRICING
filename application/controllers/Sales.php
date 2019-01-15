e1c9b62a3ecc5af11d13f0abcf6bf73fe5e421e7
fromansyah <45254852+fromansyah@users.noreply.github.com>
2019-01-15 09:49:46 +0700

    Update Sales.php

--- application/controllers/Sales.php
+++ application/controllers/Sales.php
@@ -18,41 +18,51 @@ class Sales extends CI_Controller {
         $this->load->model('Cust_site_model', 'Cust_site_model');
         $this->load->model('Employee_model', 'Employee_model');
         $this->load->model('Product_price_model', 'Product_price_model');
-        $this->load->model('Master_data_model', 'Master_data_model'); 
+        $this->load->model('Master_data_model', 'Master_data_model');
+        $this->load->model('Users_model', 'Users_model'); 
     }
     
     public function index(){
-        $this->load->helper('url');
+        $check = $this->Users_model->getRoleMenu('index.php/Sales');
         
-        $data['page_name'] = 'Sales Management';
-        $data['product_list'] = $this->Product_model->getProductList();
-        $data['cust_list'] = $this->Customer_model->getCustomerList();
-        $data['site_list'] = array();
-        $data['sp_list'] = $this->Employee_model->getEmployeeListByPosition('SP');
-        $data['price_list'] = array();
+        if(count($check) > 0){
+            $this->load->helper('url');
         
-        $periode_list = array();
-        $periode_list[1] = 1;
-        $periode_list[2] = 2;
-        $periode_list[3] = 3;
-        $periode_list[4] = 4;
-        $periode_list[5] = 5;
-        $periode_list[6] = 6;
-        $periode_list[7] = 7;
-        $periode_list[8] = 8;
-        $periode_list[9] = 9;
-        $periode_list[10] = 10;
-        $periode_list[11] = 11;
-        $periode_list[12] = 12;
-        $data['periode_list'] = $periode_list;
+            $data['page_name'] = 'Sales Management';
+            $data['product_list'] = $this->Product_model->getProductList();
+            $data['cust_list'] = $this->Customer_model->getCustomerList();
+            $data['site_list'] = array();
+            $data['sp_list'] = $this->Employee_model->getEmployeeListByPosition('SP');
+            $data['price_list'] = array();
+            
+            $periode_list = array();
+            $periode_list[1] = 1;
+            $periode_list[2] = 2;
+            $periode_list[3] = 3;
+            $periode_list[4] = 4;
+            $periode_list[5] = 5;
+            $periode_list[6] = 6;
+            $periode_list[7] = 7;
+            $periode_list[8] = 8;
+            $periode_list[9] = 9;
+            $periode_list[10] = 10;
+            $periode_list[11] = 11;
+            $periode_list[12] = 12;
+            $data['periode_list'] = $periode_list;
+            
+            $currency = array();
+            $currency['IDR'] = 'IDR';
+            $currency['USD'] = 'USD';
+            $data['currency_list'] = $currency;
+            
+            $data['content'] = $this->load->view('sales/list', $data, TRUE);
+            $this->load->view($this->session->userdata("template"), $data);
+        }else{
+            $data['title'] = 'Error Page';
+        	$data["content"] = $this->load->view('error',$data,true);
+            $this->load->view("blank", $data);
+        }
         
-        $currency = array();
-        $currency['IDR'] = 'IDR';
-        $currency['USD'] = 'USD';
-        $data['currency_list'] = $currency;
-        
-        $data['content'] = $this->load->view('sales/list', $data, TRUE);
-        $this->load->view($this->session->userdata("template"), $data);
     }
  
     public function ajax_list()
@@ -267,9 +277,18 @@ class Sales extends CI_Controller {
     }
     
     public function new_upload_sales(){
-        $data['page_title'] = 'Upload Sales';
-        $data['content'] = $this->load->view('sales/new_upload_sales', $data, TRUE);
-        $this->load->view('form_template', $data);
+        $check = $this->Users_model->getRoleMenu('index.php/Sales');
+        
+        if(count($check) > 0){
+            $data['page_title'] = 'Upload Sales';
+            $data['content'] = $this->load->view('sales/new_upload_sales', $data, TRUE);
+            $this->load->view('form_template', $data);
+        }else{
+            $data['title'] = 'Error Page';
+        	$data["content"] = $this->load->view('error',$data,true);
+            $this->load->view("blank", $data);
+        }
+        
     }
     
     public function ajax_upload()
