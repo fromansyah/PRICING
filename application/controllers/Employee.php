5c8d758d4ffdec8489b87483f860f79c3eb5f304
fromansyah <45254852+fromansyah@users.noreply.github.com>
2019-01-15 10:37:30 +0700

    Update Employee.php

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -67,9 +67,11 @@ class Employee extends CI_Controller {
             $row[] = '['.$emp->subscribe.'] '.$subscribe;
  
             //add html for action
-            $button = '<a href=\'#\' onclick="edit_emp(\''.$emp->emp_id.'\')"><img border=\'0\' src=\''.$this->config->item('base_url').'/images/file_edit.png\' title=\'Edit Employee\'></a>'.'&nbsp&nbsp&nbsp'.
-                      '<a href=\'#\' onclick="delete_emp(\''.$emp->emp_id.'\''.',\''.$emp->emp_name.'\')"><img border=\'0\' src=\''.$this->config->item('base_url').'/images/file_delete.png\' title=\'Delete Employee\'></a>';
-            
+	    $button = '';
+            if($this->session->userdata("role") == 1 || $this->session->userdata("role") == 3){
+		    $button = '<a href=\'#\' onclick="edit_emp(\''.$emp->emp_id.'\')"><img border=\'0\' src=\''.$this->config->item('base_url').'/images/file_edit.png\' title=\'Edit Employee\'></a>'.'&nbsp&nbsp&nbsp'.
+			      '<a href=\'#\' onclick="delete_emp(\''.$emp->emp_id.'\''.',\''.$emp->emp_name.'\')"><img border=\'0\' src=\''.$this->config->item('base_url').'/images/file_delete.png\' title=\'Delete Employee\'></a>';
+	    }
             $row[] = $button;
 
             $data[] = $row;
