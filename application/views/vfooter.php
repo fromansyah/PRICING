6dae69edab08749a5b6d64823f3b3ec017301d5f
fromansyah <45254852+fromansyah@users.noreply.github.com>
2019-01-15 10:25:01 +0700

    Update vfooter.php

--- application/views/vfooter.php
+++ application/views/vfooter.php
@@ -1,6 +1,6 @@
 <div id="footer" align="center">
-    <a href="<?= base_url() ?>Menu_utama">Home</a> |
-    <a href="<?= base_url() ?>Main/logout">Log Out</a>
+    <a href="<?= base_url() ?>index.php/Menu_utama">Home</a> |
+    <a href="<?= base_url() ?>index.php/Main/logout">Log Out</a>
 
     <br/><br/>
     Copyright (c) 2018 Firat
