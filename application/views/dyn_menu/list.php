e792fe5479db39f64a8ce8138a608ffb51cbb0c5
fromansyah <45254852+fromansyah@users.noreply.github.com>
2019-01-15 14:58:37 +0700

    Update list.php

--- application/views/dyn_menu/list.php
+++ application/views/dyn_menu/list.php
@@ -15,7 +15,9 @@
         <font size="5">Menu Management</font>
         <br/>
         <br/>
+        <?if($this->session->userdata("role") == 1 || $this->session->userdata("role") == 3):?>
         <button class="btn btn-success" onclick="add_menu()"><i class="glyphicon glyphicon-plus"></i> Add Menu</button>
+        <?endif;?>
         <button class="btn btn-default" onclick="reload_table()"><i class="glyphicon glyphicon-refresh"></i> Reload</button>
         <br />
         <br />
